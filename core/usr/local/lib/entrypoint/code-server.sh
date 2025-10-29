e04283f5e3dcdd556661a36edeabae2f2bcd9d9f
Alessandro Gagliano <alessandro.gagliano@gmail.com>
2025-10-29 20:55:44 +0100

    ANSVIL_CODE_APP_NAME

--- core/usr/local/lib/entrypoint/code-server.sh
+++ core/usr/local/lib/entrypoint/code-server.sh
@@ -4,6 +4,7 @@ routine_init_code_server() {
   CS_AUTH_TYPE="password"
   CS_CONFIG_DIR="${ANSVIL_USER_HOME}/.config/code-server"
   CS_CONFIG_FILE="${CS_CONFIG_DIR}/config.yaml"
+  CS_CUSTOM_STRINGS_FILE="${CS_CONFIG_DIR}/custom-strings.json"
 
   if [[ ! -f "$CS_CONFIG_FILE" ]]; then
     log INFO "Generating code-server config file"
@@ -26,6 +27,25 @@ routine_init_code_server() {
     log INFO "Code-server config created: ${CS_CONFIG_FILE}"
   fi
 
+  if [[ ! -f "$CS_CUSTOM_STRINGS_FILE" ]]; then
+    log INFO "Generating code-server custom strings file"
+
+    mkdir -p "${CS_CONFIG_DIR}"
+    {
+      echo '{'
+      echo '"WELCOME": "Welcome to {{app}}",'
+      echo '"LOGIN_TITLE": "{{app}} Access Portal",'
+      echo '"LOGIN_BELOW": "Please log in to continue",'
+      # echo '"LOGIN_PASSWORD": "",'
+      echo '"PASSWORD_PLACEHOLDER": "Enter Password"'
+      echo "}"
+    } > "${CS_CUSTOM_STRINGS_FILE}"
+
+    chown "${ANSVIL_USER}:${ANSVIL_USER}" "${CS_CUSTOM_STRINGS_FILE}"
+    chmod 600 "${CS_CUSTOM_STRINGS_FILE}"
+    log INFO "Code-server config created: ${CS_CUSTOM_STRINGS_FILE}"
+  fi
+
   # Set permissions for the Code Server config directory and file
   chown -R "${ANSVIL_USER}:${ANSVIL_USER}" "${CS_CONFIG_DIR}"
   chmod 700 "${CS_CONFIG_DIR}"
@@ -36,7 +56,7 @@ routine_init_code_server() {
 routine_start_code_server() {
   log INFO "Starting code-server..."
 
-  su "${ANSVIL_USER}" -c "source /venv/bin/activate && code-server ${ANSVIL_DEFAULT_PROJECTS_PATH}" &
+  su "${ANSVIL_USER}" -c "source /venv/bin/activate && code-server --app-name '${ANSVIL_CODE_APP_NAME:-Ansvil::Code}' --i18n ${CS_CUSTOM_STRINGS_FILE} ${ANSVIL_DEFAULT_PROJECTS_PATH}" &
   CODE_SERVER_PID=$!
 
   # Verifica se il processo è partito
