468a1c43263cf2662ffbeda698fd09b52c6c7252
Alessandro Gagliano <alessandro.gagliano@gmail.com>
2025-11-09 21:04:52 +0100

    localtime fix

--- core/usr/local/lib/entrypoint/env.sh
+++ core/usr/local/lib/entrypoint/env.sh
@@ -1,6 +1,16 @@
 routine_init_env() {
+  
   local env_path="/etc/profile.d/env.sh"
   env | grep -E '^(ANSVIL_|SEMAPHORE_|CODE_)' | sed 's/^/export /' > "${env_path}"
   echo "PATH=\"\$PATH:/usr/local/bin\"" >> "${env_path}"
   chmod 644 "${env_path}"
+
+  TZ="${TZ:-UTC}"
+
+  if [ -f "/usr/share/zoneinfo/$TZ" ]; then
+      ln -snf "/usr/share/zoneinfo/$TZ" /etc/localtime
+  else
+      log ERROR "Invalid timezone specified: $TZ."
+  fi
+
 }
